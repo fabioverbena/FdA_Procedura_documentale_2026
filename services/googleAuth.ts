0839a5163962d1b2b9c659543085e46bbfa1b66d
fabioverbena <fabioverbena@gmail.com>
2026-02-11 00:31:30 +0100

    backup: baseline before FdA migration

--- services/googleAuth.ts
+++ services/googleAuth.ts
@@ -9,6 +9,7 @@ const GOOGLE_CONFIG = {
   redirectUri: window.location.origin,
   scopes: [
     'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/gmail.send',
     'https://www.googleapis.com/auth/gmail.readonly',
